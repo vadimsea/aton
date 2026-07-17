097c3f158ec62f3aad2ac0dde4badb0ff1f41588
Vadzim <vadimsea14@gmail.com>
2026-07-17 21:08:46 +0300

    fix: set iOS article social preview image

--- site-aten/aten-seo-preview.php
+++ site-aten/aten-seo-preview.php
@@ -4,21 +4,39 @@
  * inject the site-wide default image after page metadata is prepared.
  */
 add_action('template_redirect', static function (): void {
-    if (!is_page('aten')) {
+    if (!is_page('aten') && !is_single('aton-gotovitsya-k-ios-pervaya-versiya-uzhe-testiruetsya-na-iphone')) {
         return;
     }
 
     ob_start(static function (string $html): string {
-        $old_image = 'https://vadzim.by/wp-content/uploads/2025/07/vadzim_og_viber.jpg';
-        $new_image = 'https://vadzim.by/wp-content/uploads/aten/aten-og.png';
+        $old_images = [
+            'https://vadzim.by/wp-content/uploads/2025/07/vadzim_og_viber.jpg',
+            'https://vadzim.by/wp-content/uploads/2025/07/vadzim_og_blog_viber.jpg',
+        ];
+        $new_image = is_single('aton-gotovitsya-k-ios-pervaya-versiya-uzhe-testiruetsya-na-iphone')
+            ? 'https://vadzim.by/wp-content/uploads/2026/07/Aten-ios-1.webp'
+            : 'https://vadzim.by/wp-content/uploads/aten/aten-og.png';
 
-        $html = str_replace($old_image, $new_image, $html);
+        $html = str_replace($old_images, $new_image, $html);
 
         $html = preg_replace(
             '/(<meta\s+(?:property|name)=["\'](?:og:image:type|twitter:image:type)["\']\s+content=)["\']image\/jpeg["\']/i',
-            '$1"image/png"',
+            is_single('aton-gotovitsya-k-ios-pervaya-versiya-uzhe-testiruetsya-na-iphone') ? '$1"image/webp"' : '$1"image/png"',
             $html
         ) ?? $html;
+
+        if (is_single('aton-gotovitsya-k-ios-pervaya-versiya-uzhe-testiruetsya-na-iphone')) {
+            $html = preg_replace(
+                '/(<meta\s+property=["\']og:image:width["\']\s+content=)["\']\d+["\']/i',
+                '$1"1254"',
+                $html
+            ) ?? $html;
+            $html = preg_replace(
+                '/(<meta\s+property=["\']og:image:height["\']\s+content=)["\']\d+["\']/i',
+                '$1"1254"',
+                $html
+            ) ?? $html;
+        }
 
         return $html;
     });
